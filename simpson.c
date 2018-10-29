61f3ae1385f6256fd801ee2766d48799b9fdc8e8
lxb1226 <796413852L>
2018-10-29 18:16:44 +0800

    第五次数值分析作业

---
+++ simpson.c
@@ -0,0 +1,27 @@
+//
+// Created by heyjude on 18-10-25.
+//
+
+#include<stdio.h>
+#include<math.h>
+
+//float f(float x);
+//
+//int main(void){
+//    float b=2.0,a=0.0;
+//    int m=5;
+//    float h=(b-a)/(2*m);
+//    int i;
+//    float sum=0;
+//    for(i=1;i<=m;i++){
+//        sum+=4*f(a+h*(2*i-1))+2*f(a+2*i*h);
+//
+//    }
+//    sum=h/6*(f(a)+f(b)+sum);
+//    printf("sum = %f\n",sum);
+//    return 0;
+//}
+//
+//float f(float x){
+//    return sin(x)*exp(-x*x);
+//}
